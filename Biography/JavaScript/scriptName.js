65151be45d1c29c5b10be0570fd7cc75f50c2a92
Miguel Rosas <miguelrosas@Administrators-MacBook-Pro-4.local>
2017-08-02 16:56:02 -0400

    added files

---
+++ Biography/JavaScript/scriptName.js
@@ -0,0 +1,11 @@
+//Check the form to see of the right information was passed
+//create a function that'll take in the form
+function checkForm() {
+ // create a variable that stores in the form
+ var form = ;
+ //check the form if empty, if so, give an alert, return false...
+ if (form == "") {
+   alert("You need to provide the right information");
+   return false;
+ }
+}
